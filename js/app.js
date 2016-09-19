fcfeb974c700786bc3cca7230e1b62d306fc9fbf
Gell <ngell@mel0mac534.ap.corp.jwt.com>
2016-09-19 17:34:26 +1000

    removed icon functionality.

--- js/app.js
+++ js/app.js
@@ -17,17 +17,31 @@ function changeMeasurement() {
     console.log(measurement);
 
   //!!!!! Need to update the value of temp when measurmennt change button is clicked !!!!!
-    if (id == "celsius") {
-
-    } else {
-      temp.innerHTML =
-    }
+    // if (id == "celsius") {
+    //
+    // } else {
+    //   temp.innerHTML =
+    // }
 
   }
 
 
 
 }
+
+// function updateBackground(lat, lon, temp) {
+//
+//   var latLng = new google.maps.LatLng(lat, lon);
+//
+//   var mapOptions = {
+//     zoom: 16,
+//     streetViewControl: false,
+//     scaleControl, false,
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+//   }
+//
+// }
+
 
 function geoFindMe() {
   var city = document.getElementById('city');
@@ -45,7 +59,7 @@ function geoFindMe() {
 
     var xhr = new XMLHttpRequest();
     // add ap id key
-    xhr.open("GET", "http://api.openweathermap.org/data/2.5/weather?lat=" + lat +"&lon=" + lon + "&appid=", true);
+    xhr.open("GET", "http://api.openweathermap.org/data/2.5/weather?lat=" + lat +"&lon=" + lon + "&appid=60f313ed8c7e359e0d54e58439156474", true);
     xhr.onload = function() {
       var jsonData = JSON.parse(xhr.response);
       city.innerHTML = jsonData.name;
@@ -54,6 +68,8 @@ function geoFindMe() {
       } else {
         temp.innerHTML = Math.floor(jsonData.main.temp * (9 / 5) - 459.67) + "&#176;C";
       }
+
+      // updateBackground(lat, lon, jsonData.main.temp);
 
     }
     xhr.send();
